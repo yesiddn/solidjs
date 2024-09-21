13438864efb8850867434f62e721018c11915470
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-09-20 20:41:24 -0500

    Add use of reactive primitives: signals

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import { createSignal } from 'solid-js';
+
+// const [count, setCount] = createSignal(0);
+// console.log(count());
+
+// setCount(count() + 1);
+// console.log(count());
 
 function App() {
+  const [count, setCount] = createSignal(0);
+  
   return (
     <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+      <h1>Count: {count()}</h1>
+      <button onClick={() => setCount(count() + 1)}>Increment</button>
     </div>
   );
 }
